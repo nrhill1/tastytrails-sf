df9e37f80148f7a79de09f3af11075b3ee27d415
Nicolas Hill <nrhill1@gmail.com>
2023-05-17 16:02:36 -0700

    adding markers to Map and Map to Home component

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,31 +1,40 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Leaflet from 'leaflet';
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import "leaflet/dist/leaflet.css"
 import styles from "../../styles/Home.module.css";
 
 
-const Map = () => {
+const Map: React.FC = () => {
 
     // State Hooks
-    let [foodTrucks, setFoodTrucks] = useState([]);
+    let [foodTrucks, setFoodTrucks] = useState<any[]>([]);
 
     // Fetch data from sfgov.org
     useEffect(() => {
         fetch("https://data.sfgov.org/resource/rqzj-sfat.json")
-          .then(response => response.json())
-          .then(data => setFoodTrucks(data))
-          .catch(err => console.log(err))
+            .then(response => response.json())
+            .then(data => setFoodTrucks(data))
+            .catch(err => console.log(err))
     }, [])
 
     return (
         <div>
-            <MapContainer center={[37.7749, -122.4194]} zoom={13} scrollWheelZoom={true}>
+            <MapContainer center={[37.7749, -122.4194]} zoom={13} className="foodtruck-map" scrollWheelZoom={true}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {/* {foodTrucks ? foodTrucks.map(() => {})} */}
+                {foodTrucks ? foodTrucks.map((truck) => {
+                    return (
+                        <Marker position={[truck.longitude, truck.latitude]} key={Math.random()}>
+                            <Popup>
+                                <h3>{truck.applicant}</h3> <br/>
+                                <p>{truck.address}</p>
+                            </Popup>
+                        </Marker>
+                    )
+                }) : null}
             </MapContainer>
         </div>
     )
--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,5 +1,9 @@
 import dynamic from "next/dynamic";
 
+
 const Map = dynamic(() => import("./Map"), {
-    ssr: false
+    ssr: false,
+    loading: () => <p>Loading...</p>
 })
+
+export default Map;
