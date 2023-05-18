bc4e9b0b9dab0adcd5553a105bf2c3506b2a39fa
Nicolas Hill <nrhill1@gmail.com>
2023-05-18 14:14:00 -0700

    name for the application is now StreetEats SF

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <h1>StreetEats SF</h1>
       <body className={inter.className}>{children}</body>
     </html>
   )
