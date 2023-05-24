dfe69f375dde9905e2d523c7e83cd6d0cb0d76cd
Nicolas Hill <nrhill1@gmail.com>
2023-05-24 11:54:57 -0700

    geocoder is functioning

---
+++ components/Map/LeafletControlGeocoder.ts
@@ -0,0 +1,41 @@
+// ./components/Map/LeafletControlGeocoder.ts
+
+// React imports
+import { useEffect } from 'react';
+
+// Leaflet, Leaflet Control Geocoder & react-leaflet imports
+import * as L from 'leaflet';
+import { Geocoder, geocoders }from 'leaflet-control-geocoder';
+import { useMap } from 'react-leaflet'
+
+// LCG CSS imports
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+
+
+function LeafletControlGeocoder() {
+  const map = useMap();
+
+  useEffect(() => {
+    var gc = new Geocoder({
+      geocoder: new geocoders.Nominatim(),
+      position: "topleft",
+      placeholder: "Search here...",
+      defaultMarkGeocode: false,
+      showUniqueResult: true
+    })
+    .on("markgeocode", function (e) {
+      var latlng = e.geocode.center;
+      L.marker(latlng)
+        .addTo(map)
+        .bindPopup(e.geocode.name)
+        .openPopup();
+      map.fitBounds(e.geocode.bbox);
+    })
+    gc.addTo(map);
+  }, [map]);
+
+  return null;
+}
+
+export default LeafletControlGeocoder;
--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,28 +1,33 @@
 // ./components/Map/Map.tsx
 "use client";
 
+// IMPORTS
 import { useEffect, useState } from 'react';
-import { icon } from "leaflet"
+import { icon }from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import LeafletControlGeocoder from './LeafletControlGeocoder';
 
-// Import Leaflet Control Geocoder
-import "leaflet-control-geocoder/dist/Control.Geocoder.css";
-import "leaflet-control-geocoder/dist/Control.Geocoder.js";
-
+// Import Leaflet CSS
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
 
+// Import custom CSS
 import styles from "../../styles/Map.module.css"
 
 
-const ICON = icon({
+// Icon for Food Truck
+const TRUCK_ICON = icon({
   iconUrl: "marker.png",
-  iconSize: [18, 22]
+  iconSize: [28, 36]
 })
 
+// This function formats an address properly before it's displayed in the Leaflet popup
+function addressFormatter(str: String) {
+    return str.toLowerCase().replace(/\b\w/g, s => s.toUpperCase()) + "."
+}
 
-const Map: React.FC = () => {
+const Map: React.FC = (props) => {
 
     // State Hooks
     const [foodTrucks, setFoodTrucks] = useState<any[]>([]);
@@ -34,7 +39,7 @@ const Map: React.FC = () => {
             mode: "cors",
             headers: {
                 "Content-Type": "application/json",
-                "X-App-Token": "hm9tRHSsO3Rwiveun3u02Wath"
+                "X-App-Token": process.env.SFGOV_APP_TOKEN as string
             },
         })
         .then(response => (response.json()))
@@ -49,26 +54,31 @@ const Map: React.FC = () => {
         <div>
             <MapContainer
                 center={[37.7749, -122.4194]} zoom={16} scrollWheelZoom={true}
-                style={{ width: '100%', height: '620px', margin: '0 auto', border: '1.5px whitesmoke solid', borderRadius: '10px', marginBottom: '20px' }}
+                style={{ color: 'black', width: '100%', height: '620px', margin: '0 auto', border: '2.5px whitesmoke solid', borderRadius: '10px', marginBottom: '20px' }}
             >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
                 {foodTrucks.length > 0 ? foodTrucks.map((truck) => {
-                    return (
-                        <Marker
-                            position={[parseFloat(truck.latitude), parseFloat(truck.longitude)]}
-                            key={Math.random()}
-                            icon={ICON}
-                        >
-                            <Popup>
-                                <h3><b>{truck.applicant}</b></h3>
-                                <p>Address: {truck.address}</p>
-                            </Popup>
-                        </Marker>
-                    )
+                    if (truck.address.toLowerCase() == "900 beach st"){
+                        return null
+                    } else {
+                        return (
+                            <Marker
+                                position={[parseFloat(truck.latitude), parseFloat(truck.longitude)]}
+                                key={Math.random()}
+                                icon={TRUCK_ICON}
+                            >
+                                <Popup className={styles.item_popup}>
+                                    <h3><b><u>{truck.applicant}</u></b></h3>
+                                    <p><b>Location:</b> {addressFormatter(truck.address)}</p>
+                                </Popup>
+                            </Marker>
+                        )
+                    }
                 }) : null}
+                <LeafletControlGeocoder />
             </MapContainer>
         </div>
     )
