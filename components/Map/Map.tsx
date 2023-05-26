54e5b84cd932e77deb4e665196e66e89892e75ae
Nicolas Hill <nrhill1@gmail.com>
2023-05-25 22:03:17 -0700

    google map is showing up on the page. however i'm having issues with the API

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -2,7 +2,8 @@
 "use client";
 
 // IMPORTS
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import { useLoadScript, GoogleMap, Marker } from '@react-google-maps/api';
 
 // Import custom CSS
 import styles from "../../styles/Map.module.css"
@@ -13,32 +14,85 @@ function addressFormatter(str: String) {
 }
 
 const Map: React.FC = (props) => {
+  // State Hooks
+  const [foodTrucks, setFoodTrucks] = useState<any[]>([]);
 
-    // State Hooks
-    const [foodTrucks, setFoodTrucks] = useState<any[]>([]);
+  // Fetch data from sfgov.org
+  useEffect(() => {
+    fetch("https://data.sfgov.org/resource/rqzj-sfat.json", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "X-App-Token": process.env.SFGOV_APP_TOKEN as string
+      },
+    })
+    .then(response => (response.json()))
+    .then(data => {
+      console.log(data);
+      setFoodTrucks(data);
+    })
+    .catch(err => console.log(err))
+  }, [])
 
-    // Fetch data from sfgov.org
-    useEffect(() => {
-        fetch("https://data.sfgov.org/resource/rqzj-sfat.json", {
-            method: "GET",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-                "X-App-Token": process.env.SFGOV_APP_TOKEN as string
-            },
-        })
-        .then(response => (response.json()))
-        .then(data => {
-            console.log(data);
-            setFoodTrucks(data);
-        })
-        .catch(err => console.log(err))
-    }, [])
+  const libraries = useMemo(() => ['places'], []);
+  const mapCenter = useMemo(
+    () => ({ lat: -122.4194, lng: 37.7749 }),
+    []
+  );
 
-    return (
-        <div>
-        </div>
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    [] 
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY as string,
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return(
+      <h2 id="loading" className="animate-bounce text-3xl font-bold text-center align-middle">
+        Loading...
+      </h2>
     )
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        options={mapOptions}
+        zoom={14}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerStyle={{ width: '92.5%', height: '600px', margin: '0 auto' }}
+        onLoad={() => console.log('Map Component Loaded...')}
+      >
+        {foodTrucks.length > 0 ? foodTrucks.map((truck) => {
+          if (truck.address.toLowerCase() == "900 beach st"){
+            return null
+          } else {
+            return (
+              <Marker
+                position={{lat: parseFloat(truck.latitude), lng: parseFloat(truck.longitude)}}
+                key={Math.random()}
+                // icon={TRUCK_ICON}
+              >
+                {/* <Popup className={styles.item_popup}>
+                  <h3><b><u>{truck.applicant}</u></b></h3>
+                  <p><b>Location:</b> {addressFormatter(truck.address)}</p>
+                </Popup> */}
+              </Marker>
+            )
+          }
+        }) : null}
+      </GoogleMap>
+    </div>
+  )
 }
 
 export default Map
--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -2,11 +2,10 @@
 'use client';
 
 import dynamic from "next/dynamic";
-import loading from "../../app/loading"
+
 
 const Map = dynamic(() => import("./Map"), {
     ssr: false,
-    loading: loading,
 })
 
 export default Map;
