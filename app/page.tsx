1b0528b842ce2484a735cd7400188919281e6b38
Nicolas Hill <nrhill1@gmail.com>
2023-05-30 15:09:56 -0700

    changing spacing on page.tsx to 2 spaces

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ import Navbar from "../components/Navbar/Navbar";
 
 
 export default function Home() {
-    return (
-        <div>
-            <div className="map_container">
-                <h1 className="transition text-5xl font-bold underline hover:animate-psychedelic">
-                    TastyTrails SF
-                </h1>
-                <Navbar />
-                <Map />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="map_container">
+        <h1 className="transition text-5xl font-bold underline hover:animate-psychedelic">
+          TastyTrails SF
+        </h1>
+        <Navbar />
+        <Map />
+      </div>
+    </div>
+  );
 }
