ad3077e139ffe9a6cd71b4ce28d06970d1db2edb
Nicolas Hill <nrhill1@gmail.com>
2023-05-30 13:09:10 -0700

    adding in navbar and link to github repo

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ import Navbar from "../components/Navbar/Navbar";
 export default function Home() {
     return (
         <div>
-            <Navbar />
             <div className="map_container">
                 <h1 className="transition text-5xl font-bold underline hover:animate-psychedelic">
                     TastyTrails SF
                 </h1>
+                <Navbar />
                 <Map />
             </div>
         </div>
