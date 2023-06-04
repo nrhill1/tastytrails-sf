6f0a963a434e9f26e97e1bd04003af4a0e6565e8
Nicolas Hill <nrhill1@gmail.com>
2023-06-03 17:00:16 -0700

    removing AuthButton references from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 "use client";
 
 // Component Imports
-import AuthButton from '../components/AuthButton'
 import Header from '../components/Header'
 import Map from "../components/Map";
 import Navbar from "../components/Navbar";
@@ -15,7 +14,6 @@ export default function Home() {
   return (
     <div>
       <Header />
-      {/* <AuthButton /> */}
       <div className="map_container">
         <Navbar />
         <Map />
